298feefd8bbbecc9f0e9e653205c8b85bf2d34c1
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-03-27 19:08:26 +0530

    Review Comments

--- js/apicall.js
+++ js/apicall.js
@@ -2,4 +2,4 @@ import instance from './news.js';
 const apiKey = "720c0314e8b2423eb7e1ffca5a1eeeb1";
 const newsCategory = "bbc-news";
 const url = `https://newsapi.org/v1/articles?source=${newsCategory}&apiKey=${apiKey}`;
-instance.fetchResults(url);
+instance.apiFetcher(url);
--- js/news.js
+++ js/news.js
@@ -1,23 +1,37 @@
-
-'use strict';
+"use strict";
 class fetchdata {
-  fetchResults(url) {
+  apiFetcher(url) {
     let globalData = {};
     fetch(url)
       .then(response => response.json())
-      .then(myJson => instance.printResults(myJson))
-      .catch(error => { console.log("Something went wrong on server") });
+      .then(myJson => newsSource.articlesProvider(myJson))
+      .catch(error => console.log("Something went wrong on server"));
   }
-  printResults(data) {
-    const elementId = document.getElementById('newsDetails');
+}
+
+class newsSourceProvider extends fetchdata {
+  constructor(data) {
+    super(data);
+  }
+  articlesProvider(data) {
+    const elementId = document.getElementById("newsDetails");
     let returnHtml = "",
       uniqueVal = [];
     data.articles.map((article, index) => {
       uniqueVal = index === 0 ? `<h1>${article.author}</h1>` : "";
-      returnHtml += `${uniqueVal}<div class="newsTitle">${article.title}</div><div class="newsDescription">${article.description}</div><div class="publishDate">${article.publishedAt}</div><div class="imageContainer"><a href="${article.url}" target="_blank"><img src=${article.urlToImage} /></a></div>`;
+      returnHtml += `${uniqueVal}<div class="newsTitle">${
+        article.title
+      }</div><div class="newsDescription">${
+        article.description
+      }</div><div class="publishDate">${
+        article.publishedAt
+      }</div><div class="imageContainer"><a href="${
+        article.url
+      }" target="_blank"><img src=${article.urlToImage} /></a></div>`;
     });
-    elementId.insertAdjacentHTML('afterbegin', returnHtml);
+    elementId.insertAdjacentHTML("afterbegin", returnHtml);
   }
 }
+const newsSource = new newsSourceProvider();
 const instance = new fetchdata();
 export default instance;
